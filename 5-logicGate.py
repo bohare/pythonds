6fa2a4019e64443dd1af28d6848b5e51a54990c7
Rahul <rahul.bohare@tum.de>
2019-02-17 17:43:07 +0100

    Logic Gates Simulation

---
+++ 5-logicGate.py
@@ -0,0 +1,182 @@
+class LogicGate:
+
+    def __init__(self, n):
+        self.name = n
+        self.output = None
+
+    def getLabel(self):
+        return self.name
+
+    def getOutput(self):
+        self.output = self.performGateLogic()
+        return self.output
+
+
+class BinaryGate(LogicGate):
+
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pinA = None
+        self.pinB = None
+
+    def getPinA(self):
+        if self.pinA == None:
+            return int(input("Enter Pin A input for gate " + self.getLabel() + "-->"))
+        else:
+            return self.pinA.getFrom().getOutput()
+
+    def getPinB(self):
+        if self.pinB == None:
+            return int(input("Enter Pin B input for gate " + self.getLabel() + "-->"))
+        else:
+            return self.pinB.getFrom().getOutput()
+
+    def setNextPin(self, source):
+        if self.pinA == None:
+            self.pinA = source
+        else:
+            if self.pinB == None:
+                self.pinB = source
+            else:
+                print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class AndGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+
+class OrGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return 0
+
+
+class UnaryGate(LogicGate):
+
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pin = None
+
+    def getPin(self):
+        if self.pin == None:
+            return int(input("Enter Pin input for gate " + self.getLabel() + "-->"))
+        else:
+            return self.pin.getFrom().getOutput()
+
+    def setNextPin(self, source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class NotGate(UnaryGate):
+
+    def __init__(self, n):
+        UnaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+        if self.getPin():
+            return 0
+        else:
+            return 1
+
+
+class NorGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 0 and b == 0:
+            return 1
+        else:
+            return 0
+
+
+class NandGate(BinaryGate):
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+            return 0
+        else:
+            return 1
+
+
+class Connector:
+
+    def __init__(self, fgate, tgate):
+        self.fromgate = fgate
+        self.togate = tgate
+
+        tgate.setNextPin(self)
+
+    def getFrom(self):
+        return self.fromgate
+
+    def getTo(self):
+        return self.togate
+
+
+def main():
+    '''g1 = AndGate("G1")
+    g2 = AndGate("G2")
+    g3 = OrGate("G3")
+    g4 = NotGate("G4")
+    c1 = Connector(g1, g3)
+    c2 = Connector(g2, g3)
+    c3 = Connector(g3, g4)
+    print(g4.getOutput())'''
+
+    # This block simulates the circuit: NOT((A and B) or (C and D))
+    and_1 = AndGate('First And Gate')
+    and_2 = AndGate('Second And Gate')
+    _nor = NorGate('Final Nor Gate')
+    c1 = Connector(and_1, _nor)
+    c2 = Connector(and_2, _nor)
+    firstCircuitOutput = _nor.getOutput()
+    print('Output of first circuit is {}'.format(firstCircuitOutput))
+
+    # This block simulates the circuit: NOT(A and B) and NOT(C and D)
+    nand_1 = NandGate('First Nand Gate')
+    nand_2 = NandGate('Second Nand Gate')
+    _and = AndGate('Final And Gate')
+    c1 = Connector(nand_1, _and)
+    c2 = Connector(nand_2, _and)
+    SecondCircuitOutput = _and.getOutput()
+    print('Output of second circuit is {}'.format(SecondCircuitOutput))
+
+    # Check if the output of two circuits are equal to prove the theorem
+    if firstCircuitOutput == SecondCircuitOutput:
+        print('Theorem: NOT(( A and B) or (C and D)) = NOT(A and B) and NOT(C and D) proved!')
+
+
+main()
