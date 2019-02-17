1dadf2cb2d33cc41e4c1420e69feb332c19d24af
Rahul <rahul.bohare@tum.de>
2019-02-17 20:40:11 +0100

    Add a class to represent people on a college campus

---
+++ Chapter1_Introduction/DiscussionExercises/college.py
@@ -0,0 +1,23 @@
+class College:
+
+    def __init__(self, name, age, contact):
+        self.name = name
+        self.age = age
+        self.contact = contact
+
+    def __str__(self):
+        return 'Name: {}, Contact: {}'.format(str(self.name), str(self.contact))
+
+
+class student(College):
+    def __init__(self, name, age, contact, grade, email):
+        College.__init__(self, name, age, contact)
+        self.grade = grade
+        self.email = email
+
+    def register_for_exams(self):
+        pass
+
+
+Rahul = College('Rahul Bohare', '25', '0123456789')
+print(Rahul)
